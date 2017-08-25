5ca4d03a82d2ce1eb80706677ba501c06fb4d0e1
hambone124 <hpe60m5@gmail.com>
2017-08-25 16:11:11 -0400

    Helix AutoLogon functions

--- TervisHelix.psm1
+++ TervisHelix.psm1
@@ -35,7 +35,8 @@ function Install-HelixDowntimeClient {
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName
     )
     begin {
-        $HelixDowntimeClient = "\\tervis.prv\departments\Departments - I Drive\Shared\Operations\Chad\Helix\Helix Downtime Client.accdb"
+        $DomainName = (Get-ADDomain).DNSRoot
+        $HelixDowntimeClient = "\\$DomainName\departments\Departments - I Drive\Shared\Operations\Chad\Helix\Helix Downtime Client.accdb"
     }
     process {
         Set-Shortcut -LinkPath "\\$ComputerName\C$\Users\Public\Desktop\Helix Downtime Client.lnk" -TargetPath $HelixDowntimeClient
@@ -61,5 +62,72 @@ function Invoke-CommandOnHelixStations {
         param ($Parameter,$OptionalParameters)
         $Scriptblock = [scriptblock]::Create($OptionalParameters)
         Invoke-Command -ComputerName $Parameter -ScriptBlock $Scriptblock
+    }
+}
+
+# Only useful for Stations 01 - 12 currently until Get-HelixAutoLogonCredential is updated.
+function New-HelixUser {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)][int]$StationID
+    )
+    begin {
+        $HelixUserNameBase = "Helix"
+        $DomainName = (Get-ADDomain).DNSRoot.Split(".")
+        $OU = "OU=Users,OU=Helix,OU=IndustryPCs,DC=$($DomainName[0]),DC=$($DomainName[1])"
+    }
+    process {
+        $StationIDString = $StationID.ToString().PadLeft(2,"0")
+        $HelixUserCredential = Get-HelixAutoLogonCredential -ComputerName ("Helix" + $StationIDString + "-PC")
+        $HelixUserName = $HelixUserNameBase + $StationIDString
+        $HelixADUser =
+            New-ADUser `
+            -AccountPassword $HelixUserCredential.Password `
+            -CannotChangePassword $true `
+            -Enabled $true `
+            -SamAccountName $HelixUserName `
+            -PasswordNeverExpires $true `
+            -Name $HelixUserName `
+            -Path $OU `
+            -PassThru
+    }
+}
+
+function Set-HelixAutoLogonGPO {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)][string]$ComputerName
+    )
+    process {
+        $HelixAutoLogonGPO = Get-GPO -Name "Autologon - $($ComputerName.Substring(0,7))"
+        $HelixAutoLogonCredential = Get-HelixAutoLogonCredential -ComputerName $ComputerName
+        $GPODefaultValues = @{
+            Name = $HelixAutoLogonGPO.DisplayName
+            Context = "Computer"
+            Key = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+            Type = "String"
+            Action = "Replace"
+        }
+        Write-Verbose "Setting AutoLogon GPO for $ComputerName"
+        Set-GPPrefRegistryValue @GPODefaultValues -ValueName AutoAdminLogon -Value 1 -Order 1 | Out-Null
+        Set-GPPrefRegistryValue @GPODefaultValues -ValueName DefaultUserName -Value $HelixAutoLogonCredential.Username -Order 2 | Out-Null
+        Set-GPPrefRegistryValue @GPODefaultValues -ValueName DefaultPassword -Value $HelixAutoLogonCredential.GetNetworkCredential().Password -Order 3 | Out-Null
+    }
+}
+
+# Only applicable to Helix 01 - 12 until Get-HelixAutoLogonCredential is updated
+function Get-HelixAutoLogonCredential {
+    param (
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ComputerName
+    )
+    begin {
+        $PIDZero = 4898
+    }
+    process {
+        $StationID = $ComputerName.Substring(5,2)
+        $IDInt = [int]::Parse($StationID)
+        if (($IDInt -lt 1) -or ($IDInt -gt 12)) {
+            throw "Get-HelixAutoLogonCredential does not yet support getting credentials for stations numbers greater than 12"
+        }
+        $PasswordID = $PIDZero + $IDInt
+        Get-PasswordstateCredential -PasswordID $PasswordID
     }
 }
